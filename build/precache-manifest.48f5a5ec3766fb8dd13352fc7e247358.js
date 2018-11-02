bcc7d0645262aab8b28c31b56b83f7fac100ff35
Marcin Koczorowski <marcin171194@wp.pl>
2018-11-02 15:09:32 +0100

    initial commit

---
+++ build/precache-manifest.48f5a5ec3766fb8dd13352fc7e247358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3122dfef998e7a887ec8",
+    "url": "/static/js/main.3122dfef.chunk.js"
+  },
+  {
+    "revision": "69632da2307669b97046",
+    "url": "/static/js/1.69632da2.chunk.js"
+  },
+  {
+    "revision": "3122dfef998e7a887ec8",
+    "url": "/static/css/main.b95bde3d.chunk.css"
+  },
+  {
+    "revision": "c3646b8ce870e74928f78be335c07cc3",
+    "url": "/index.html"
+  }
+];
